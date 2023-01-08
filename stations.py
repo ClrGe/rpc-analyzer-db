0b19a2fa7fea32d3c0324875881a63114a4ba51a
Lokda-spec <gouarnec@gmail.com>
2023-01-08 15:27:21 +0100

    separated query file

--- stations.py
+++ stations.py
@@ -19,19 +19,19 @@ class Station(object):
         rslt = exe.fetchall()
         print(rslt)
 
-        i = 0
         result = []
+        i = 0
         for i in rslt:
             response =  stations_pb2.readResponsePB().Value()
             response.gare_alias_libelle = str(i[1])
-            response.gare_regionsncf = str(i[2])
-            response.adresse_cp = str(i[3])
-            response.departement = str(i[4])
-            #response.uic_code = str(i[5])
+                #response.gare_regionsncf = str(i[2])
+                #response.adresse_cp = str(i[3])
+                #response.departement = str(i[4])
+                #response.uic_code = str(i[5])
 
             result.append(response)
             
 
         response_list = stations_pb2.readResponsePB()
-        response_list.value.append(result)
+        response_list.value.extend(result)
         return response_list
